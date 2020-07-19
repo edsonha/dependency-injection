039c655c6fcb1a5304773049b73dba3c3699e9ac
edsonha <edsonha@gmail.com>
2020-07-19 11:14:25 +0800

    First commit - creating OrderManager, Product and Program class

---
+++ DependencyInjection/OrderManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+	public class OrderManager
+	{
+		public void Submit(Product product, string creditCardNumber, string expiryDate)
+		{
+			Console.WriteLine($"Product: {product}, Number: {creditCardNumber}, Expiry: {expiryDate}");
+		}
+	}
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var product = string.Empty;
+			var orderManager = new OrderManager();
+			while (product != "exit")
+			{
+				Console.WriteLine(@"Enter a Product: 
+					Keyboard = 0,
+					Mouse = 1,
+					Mic = 2,
+					Speaker = 3"
+				);
+				product = Console.ReadLine();
+
+				try
+				{
+					if (Enum.TryParse(product, out Product productEnum))
+					{
+						Console.WriteLine("Please enter a valid payment method: XXXXXXXXXXXXXXXX;MMYY");
+						var paymentMethod = Console.ReadLine();
+						if (string.IsNullOrEmpty(paymentMethod) || paymentMethod.Split(";").Length != 2)
+						{
+							throw new Exception("Invalid Payment Method");
+						}
+						string creditCardNumber = paymentMethod.Split(";")[0];
+						string expiryDate = paymentMethod.Split(";")[1];
+						orderManager.Submit(productEnum, creditCardNumber, expiryDate);
+
+					}
+					else
+					{
+						Console.WriteLine("Invalid Product");
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+
+				}
+				Console.WriteLine(Environment.NewLine);
+			}
+		}
+	}
+}
