b652014810a10aa963acaaf933d4366d5b9dc193
edsonha <edsonha@gmail.com>
2020-07-19 14:28:41 +0800

    Install Moq and add unit test to mock no stock and should return exception error

--- DependencyInjectionTests/OrderManagerTests.cs
+++ DependencyInjectionTests/OrderManagerTests.cs
@@ -1,4 +1,5 @@
 using DependencyInjection;
+using Moq;
 using System;
 using Xunit;
 
@@ -9,8 +10,16 @@ namespace DependencyInjectionTests
 		[Fact]
 		public void Test1()
 		{
-			var orderManager = new OrderManager();
-			orderManager.Submit(Product.Keyboard, "1234", "1234");
+			var productStockRepositoryMock = new Mock<IProductStockRepository>();
+			productStockRepositoryMock
+				.Setup(m => m.IsInStock(It.IsAny<Product>())) //Or Product.Keyboard, etc
+				.Returns(false);
+
+			var paymentProcessorMock = new Mock<IPaymentProcessor>();
+			var shippingProcessorMock = new Mock<IShippingProcessor>();
+
+			var orderManager = new OrderManager(productStockRepositoryMock.Object, paymentProcessorMock.Object, shippingProcessorMock.Object);
+
 			Assert.ThrowsAny<Exception>(() => orderManager.Submit(Product.Keyboard, "1234", "1234"));
 		}
 	}
